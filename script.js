354243eebab5fc2b10e787fc0ea9bfcfb65f42e3
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2024-10-16 22:47:58 +0300

    starting

---
+++ script.js
@@ -0,0 +1,30 @@
+const engYears={
+    "Freshman":String,
+    "1st":String,
+    "2nd":String,
+    "3rd":String,
+    "4th":String,
+};
+const medYears={
+    "1st":String,
+    "2nd":String,
+    "3rd":String,
+    "4th":String,
+     "5th":String,
+     "6th":String,
+     "7th":String,   
+};
+const PharmYears={
+    "1st":String,
+    "2nd":String,
+    "3rd":String,
+    "4th":String,
+    "5th":String,   
+};
+const comYears=
+{
+    "1st":String,
+    "2nd":String,
+    "3rd":String,
+    "4th":String, 
+}
